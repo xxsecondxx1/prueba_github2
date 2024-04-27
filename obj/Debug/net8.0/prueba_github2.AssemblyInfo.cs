482a2976d3c172d4b418b447f73636acccd8b87e
YanpieroCutipa <yanpiero_cutipa@usmp.pe>
2024-04-27 00:09:47 -0500

    Mayron

--- obj/Debug/net8.0/prueba_github2.AssemblyInfo.cs
+++ obj/Debug/net8.0/prueba_github2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba_github2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45158c228dad3b75cb03d470de7e021ce64a5763")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3db98db81f139d9110f3732dfd31a095cd3e416")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba_github2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba_github2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
